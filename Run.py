baa401c4d8a1c4dbe21ba82d19e2aa75d943b36f
dryzrlbs <deryaergun@gmail.com>
2026-01-12 20:05:04 +0200

    run_py

--- Run.py
+++ Run.py
@@ -1,5 +1,5 @@
-from preprocessing import preprocess
-from modeling import run_lgbm_cv
+from Preprocessing import preprocess
+from Modeling import run_lgbm_cv
 
 X, y, features = preprocess("train.csv")
 run_lgbm_cv(X, y)
